563ff6028182d1de671f743c2431808ba31e5cc2
Simon Coenen <sim2.coenen@gmail.com>
2020-08-29 21:04:37 +0100

    Dark theme and ToC

--- js/main.js
+++ js/main.js
@@ -31,3 +31,43 @@ window.onscroll = function () {
 	}
 	prevScrollPos = currentScrollPos;
 };
+
+//Table of content highlighting
+
+$(document).ready(function() {
+	$(window).scroll(function() {
+	  $("#table-of-contents a").removeClass("current")
+	  currentAnchor().addClass("current")
+	})
+  });
+  
+  function tocItem(anchor) {
+	return $("[href=\"" + anchor + "\"]")
+  }
+  
+  function heading(anchor) {
+	return $("[id=" + anchor.substr(1) + "]")
+  }
+  
+  var _anchors = null
+  function anchors() {
+	if (!_anchors) {
+	  _anchors = $("#table-of-contents a").map(function() {
+		return $(this).attr("href")
+	  })
+	}
+	return _anchors
+  }
+  
+  function currentAnchor() {
+	var winY = window.pageYOffset
+	var currAnchor = null
+	anchors().each(function() {
+	  var y = heading(this).position().top
+	  if (y < winY + window.innerHeight * 0.1) {
+		currAnchor = this
+		return
+	  }
+	})
+	return tocItem(currAnchor)
+  }
