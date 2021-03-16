d1942d1065af077960bd576f0962547cf916ad6b
Simon Coenen <sim2.coenen@gmail.com>
2021-03-16 19:52:36 +0000

    Minor update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-$('.video-overlay').on({
-	mouseenter: function () {
-		$(this).find('video').get(0).pause();
-		$(this).find('video').get(0).currentTime = 0;
-		$(this).find('video').get(0).play();
-	},
-	mouseleave: function () {
-		$(this).find('video').get(0).pause();
+// Play video on hover
+$(document).ready(function() {
+	
+	$(".video-overlay").hover(hoverVideo, hideVideo);
+
+	function hoverVideo(e) {
+		$('video', this).get(0).currentTime = 0;
+		$('video', this).get(0).play();
+	}
+	function hideVideo(e) {
+		$('video', this).get(0).pause();
 	}
 });
 
+// Hide navbar when scrolling down
 var prevScrollPos = window.pageYOffset;
 window.onscroll = function () {
 	var currentScrollPos = window.pageYOffset;
@@ -23,41 +27,53 @@ window.onscroll = function () {
 };
 
 //Table of content highlighting
-
 $(document).ready(function() {
+	
+	$("#table-of-contents a").removeClass("current")
+	currentAnchor().addClass("current")
+
 	$(window).scroll(function() {
 	  $("#table-of-contents a").removeClass("current")
 	  currentAnchor().addClass("current")
 	})
-  });
+});
   
-  function tocItem(anchor) {
-	return $("[href=\"" + anchor + "\"]")
-  }
-  
-  function heading(anchor) {
-	return $("[id=" + anchor.substr(1) + "]")
-  }
-  
-  var _anchors = null
-  function anchors() {
+function tocItem(anchor) {
+return $("[href=\"" + anchor + "\"]")
+}
+
+function heading(anchor) {
+return $("[id=" + anchor.substr(1) + "]")
+}
+
+function anchors() {
 	if (!_anchors) {
-	  _anchors = $("#table-of-contents a").map(function() {
-		return $(this).attr("href")
-	  })
+		_anchors = $("#table-of-contents a").map(function() {
+			return $(this).attr("href")
+		})
 	}
 	return _anchors
-  }
-  
-  function currentAnchor() {
+}
+
+var _anchors = null
+function currentAnchor() {
 	var winY = window.pageYOffset
 	var currAnchor = null
-	anchors().each(function() {
-	  var y = heading(this).position().top
-	  if (y < winY + window.innerHeight * 0.1) {
-		currAnchor = this
-		return
-	  }
-	})
+
+	if (!_anchors) {
+		_anchors = $("#table-of-contents a").map(function() {
+			return $(this).attr("href")
+		})
+	}
+	_anchors.each(function(index, value) {
+		var y = heading(value).position().top
+		if (y < winY) {
+			currAnchor = value;
+			return;
+		}
+	});
+	if(currAnchor == null) {
+		currAnchor = _anchors[0];
+	}
 	return tocItem(currAnchor)
-  }
+}
